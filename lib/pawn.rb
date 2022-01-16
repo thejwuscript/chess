f47fc005c6c16f10d3a8c58ca0bcf55d059ffccb
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-01-16 13:48:10 +0000

    Create two tests for class Pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class Pawn
-  def initialize(name, color, position = nil)
-    @name = name
+  attr_reader :position
+  
+  def initialize(color, position)
+    @symbol = color == 'B' ? '♟︎' : '♙'
     @color = color
     @position = position
   end
-    
+
+  def update_position_to(position)
+    @position = position
+  end
+
 end
--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -3,4 +3,21 @@
 require_relative '../lib/pawn'
 
 RSpec.describe Pawn do
+  describe '#update_position_to' do
+    subject(:moved_pawn) { described_class.new('B', 'D1') }
+    
+    it 'updates @position to the position given as an argument' do
+      new_position = 'H1'
+      moved_pawn.update_position_to(new_position)
+      expect(moved_pawn.position).to eql(new_position)
+    end
+  end
+
+  describe '#initialize' do
+    context 'when color of the piece is white' do
+      it 'assigns ♙ to @symbol' do
+      end
+    end
+  end
+    
 end
