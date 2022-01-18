f9c8082fcef982afdece95f95b3e5689ff457a0f
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-01-18 18:13:35 +0000

    Add methods for initial setup

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 class Game
-  attr_accessor :board
+  attr_reader :board, :all_pieces
 
   def initialize
     @board = Board.new
+    @all_pieces = arrange_all_pieces
+    set_initial_positions
   end
 
-  def place_on_board(piece)
-    board.set_piece_at(piece.position, piece)
+  def set_initial_positions
+    all_pieces.each do |key, value|
+      value.each { |piece| board.set_piece_at(piece.position, piece) }
+    end
   end
 
   def create_all_pieces(array = [])
@@ -16,6 +20,14 @@ class Game
     4.times { array.push(Rook.new, Bishop.new, Knight.new) }
     2.times { array.push(Queen.new, King.new) }
     array
+  end
+
+  def arrange_all_pieces
+    create_all_pieces.reduce(Hash.new) do |result, piece|
+      result.key?(piece.type) ? 
+        result[piece.type].push(piece) : result[piece.type] = [piece]
+      result
+    end
   end
 
 end
