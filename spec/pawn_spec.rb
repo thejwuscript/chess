df18c175e9dd882a6b12e8b6bd17e59314c50a78
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-01-18 15:14:01 +0000

    Test #initialize for Pawn

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -4,16 +4,44 @@ require_relative '../lib/pawn'
 require_relative '../spec/shared_example_spec'
 
 RSpec.describe Pawn do
-  include_examples 'parent class Piece methods'
-  
+  describe '#initialize' do
+    subject(:first_pawn) { described_class.new }
+    subject(:second_pawn) { described_class.new }
+    subject(:third_pawn) { described_class.new('Q') }
+    
+    it 'assigns black and position A2 to the first pawn' do
+      color = first_pawn.color
+      position = first_pawn.position
+      expect(color).to eql('B')
+      expect(position).to eql('A2')
+    end
+
+    it 'assigns white and position A7 to the second pawn' do
+      color = second_pawn.color
+      position = second_pawn.position
+      expect(color).to eql('W')
+      expect(position).to eql('A7')
+    end
+
+    context 'when third pawn takes a string as an argument' do
+      it 'assigns position B2 and the string to @color respectively' do
+        string = third_pawn.color
+        position = third_pawn.position
+        expect(string).to eql('Q')
+        expect(position).to eql('B2')
+      end
+    end
+  end
+
   describe '#update_position_to' do
     subject(:moved_pawn) { described_class.new }
     
     it 'updates @position to the position given as an argument' do
-      moved_pawn.position = 'D1'
       new_position = 'H1'
       moved_pawn.update_position_to(new_position)
       expect(moved_pawn.position).to eql(new_position)
     end
   end
+
+  include_examples 'parent class Piece methods'
 end
