34df93b35cb043e0fb42212b74c39e55df64da37
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-02-06 14:10:37 +0000

    Refactor test for #create_all_pieces

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,13 +14,9 @@ RSpec.describe Game do
   subject(:game) { described_class.new }
   
   describe '#create_all_pieces' do
-    
-    before do
-      game.create_all_pieces
-    end
-  
-    it 'stores an array of all board pieces in @all_pieces' do
-      expect(game.all_pieces).to include(a_kind_of(Pawn)).exactly(16).times
+    it 'returns an array of all board pieces' do
+      array = game.create_all_pieces
+      expect(array).to include(a_kind_of(Pawn)).exactly(16).times
        .and include(a_kind_of(Rook)).exactly(4).times
        .and include(a_kind_of(Knight)).exactly(4).times
        .and include(a_kind_of(Bishop)).exactly(4).times
@@ -28,11 +24,12 @@ RSpec.describe Game do
        .and include(a_kind_of(King)).twice
     end
 
-    it 'creates pairs of the same class from the beginning' do
+    it 'creates pairs of pieces of the same kind' do
+      array = game.create_all_pieces
       result = true
       i = 0
-      until game.all_pieces[i].nil? do
-        break result = false if game.all_pieces[i].class != game.all_pieces[i+1].class
+      until array[i].nil? do
+        break result = false if array[i].class != array[i+1].class
         
         i += 2
       end
@@ -40,10 +37,11 @@ RSpec.describe Game do
     end
 
     it 'initializes pieces that have no color, position and symbol attributes' do
-      result = game.all_pieces.all? do |piece|
+      array = game.create_all_pieces
+      result = array.all? do |piece|
         piece.color.nil? && piece.position.nil? && piece.symbol.nil?
       end
-    expect(result).to be true
+      expect(result).to be true
     end
   end
 
