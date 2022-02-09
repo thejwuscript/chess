92d530c794933233f0bb760b6916460108953cba
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-02-09 14:10:13 +0000

    Test #locate_enemy_pawn

--- lib/en_passant_checker.rb
+++ lib/en_passant_checker.rb
@@ -10,6 +10,13 @@ class EnPassantChecker
     @finding = nil
   end
 
+  def locate_enemy_pawn
+    row, column = target_ary
+    modifier = pawn.color == 'W' ? 1 : -1
+    piece = board.grid[row + modifier][column]
+    piece if piece.is_a?(Pawn) && piece.color != pawn.color
+  end
+
   def validate_capture_condition
     # create a modifier which is a value of -1 or 1 depending on the pawn's color
     
--- spec/en_passant_checker_spec.rb
+++ spec/en_passant_checker_spec.rb
@@ -1,7 +1,26 @@
 #frozen_string_literal: true
 
 require_relative '../lib/en_passant_checker'
+require_relative '../lib/game'
+require_relative '../lib/pawn'
+require_relative '../lib/board'
 
 RSpec.describe EnPassantChecker do
+  let(:board) { instance_double(Board) }
+  let(:pawn) { instance_double(Pawn, color: 'W') }
+  let(:enemy) { instance_double(Pawn, is_a?: Pawn, color: 'B') }
+  let(:game) { instance_double(Game) }
+  subject(:checker) { described_class.new(board, pawn, [2, 7], game) }
   
+  describe '#locate_enemy_pawn' do
+    context 'when an enemy pawn is found' do
+      it 'returns the enemy pawn to be evaluated for en passant capture' do
+        allow(board).to receive(:grid) {
+          [[], [], [], [nil, nil, nil, nil, nil, nil, nil, enemy], [], [], [], []] 
+          }
+        result = checker.locate_enemy_pawn
+        expect(result).to eq(enemy)
+      end
+    end
+  end
 end
