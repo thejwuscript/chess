a93119940b637704686ad017463f48f3c056c783
thejwuscript <88938117+thejwuscript@users.noreply.github.com>
2022-02-22 05:21:26 +0000

    Fix AI move

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -36,17 +36,18 @@ class ComputerPlayer
     examiners.each { |examiner| return examiner if examiner.en_passant_verified || examiner.castling_verified }
     
     filtered = examiners.reject { |examiner| danger_queen?(examiner) }
-    distilled = filtered.reject do |examiner| 
+    more_filtered = filtered.reject do |examiner| 
       danger_self?(examiner) if [Queen, Knight, Rook, Bishop].include?(examiner.piece.class)
 
     end  
-    urgent = distilled.reject { |examiner| !(danger_now?(examiner)) } unless distilled.empty?
-    captures = distilled.select { |examiner| board.piece_at(examiner.target) }
+    urgent = more_filtered.reject { |examiner| !(danger_now?(examiner)) } 
+    captures = more_filtered.select { |examiner| board.piece_at(examiner.target) }
     promote_capture = captures.find do |examiner|
       target_class = board.piece_at(examiner.target).class
       [Queen, Rook, Bishop, Knight].include?(target_class)
     end
-    promote_capture || urgent.sample || captures.sample || distilled.sample || filtered.sample || examiners.sample
+    promote_capture || urgent.sample || captures.sample || 
+    more_filtered.sample || filtered.sample || examiners.sample
   end
 
   def danger_self?(examiner, mock_board = board.deep_clone)
@@ -57,10 +58,12 @@ class ComputerPlayer
   end
 
   def danger_queen?(examiner, mock_board = board.deep_clone)
-    return if examiner.piece.is_a?(Queen) #or queen's gone
+    return if examiner.piece.is_a?(Queen)
     
     mock_board.move_piece_to_target(examiner.target, examiner.piece)
     queen = mock_board.grid.flatten.compact.find { |piece| piece.color == self.color && piece.is_a?(Queen) }
+    return if queen.nil?
+    
     mock_board.all_enemies(self.color).any? do |enemy|
       MoveExaminer.new(mock_board, enemy, queen.position).validate_move
     end
